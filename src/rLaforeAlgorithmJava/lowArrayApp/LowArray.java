651771ebd4511efa9f2d51e8ab3d6aa816803fbb
odinets <odinets@irc.beltelecom.by>
2020-10-21 09:35:33 +0300

    1

---
+++ src/rLaforeAlgorithmJava/lowArrayApp/LowArray.java
@@ -0,0 +1,18 @@
+package rLaforeAlgorithmJava.lowArrayApp;
+
+public class LowArray {
+	
+	private long[] a;
+	
+	public LowArray(int size) {
+		a = new long[size];
+	}
+	
+	public void setElement(int index, long value) {
+		a[index] = value;
+	}
+	
+	public long getElement(int index) {
+		return a[index];
+	}
+}
